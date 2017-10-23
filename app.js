3fd39cc6739dd297f3744711fc0e8ba3401f5c7a
lauralm <mein_herz_ist_kalt@hotmail.com>
2017-10-23 15:13:08 -0500

    ejemplo de array

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 var fila = [1,2,3,4];
-
+var objeto ={perrito:"perrito "};
 fila.shift();
 document.write(fila);
+fila.push("Hola");
+document.write("<br>"+fila);
+fila.pop();
+document.write("<br>"+fila);
+fila.unshift(objeto.perrito)
+document.write("<br>"+fila);
